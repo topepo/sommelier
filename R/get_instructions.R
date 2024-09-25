caf1f44ab2386a70001dc89b2ee5664412f16ed5
topepo <mxkuhn@gmail.com>
2024-09-25 10:44:51 -0400

    documentation

--- R/get_instructions.R
+++ R/get_instructions.R
@@ -1,3 +1,11 @@
+#' Extract instruction prompt
+#'
+#' Pull the instruction prompt used by the package into R.
+#' @param collapse A logical: should the instructions be returned as a single
+#' character string (the default) or a character vector.
+#' @return A character vector.
+#' @examples
+#' substring(get_instructions(), 1, 23)
 #' @export
 get_instructions <- function(collapse = TRUE) {
   file_nm <- system.file(package = "sommelier", "instruction_promt.md")
