57487b75531968963ae575ebaad423ec23f6e335
topepo <mxkuhn@gmail.com>
2024-10-09 07:06:11 -0400

    new funciton name and updated elmer code

--- R/tm_chat.R
+++ R/tm_chat.R
@@ -3,26 +3,23 @@
 #' This function uses a large language model (LLM) to process a user's questions
 #' and return instructions and code.
 #'
-#' @param model A character string for the LLM model.
-#' @return Nothing since users have to quit the discussion. However, the history
-#' of the chat is saved in `.Last.chat.result`.
+#' @details
+#'
+#' The function specifically uses Antropic's Claude tools. You will need an API
+#' key for that system. To set it, create an environmental variable named
+#' `ANTHROPIC_API_KEY` or declare it in your local projects `.Renviron` file.
+#' We suggest the latter using [usethis::edit_r_environ()].
+#'
+#' @return An object of class `Chat` (and `R6`) with the conversation.
 #' @export
-tm_chat <- function(model = "gpt-4o-mini") {
-  # TODO This needs a better name
-  # TODO Use anthropic instead
-  chat <- elmer::new_chat_openai(
-    model = model,
+recommend <- function() {
+  chat <- elmer::chat_claude(
     system_prompt = get_instructions(),
     echo = TRUE
   )
 
-  tryCatch(
-    chat$console(),
-    interrupt = function(cnd) NULL
-  )
+  elmer::live_browser(chat)
 
   invisible(chat)
 }
 
-# TODO
-# - general human factors/user interaction issues thought out
